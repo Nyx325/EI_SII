b34467c78ff03f4300c193b1b8b66a3af9fd648d
Omar Roman <omar.roman.2828@gmail.com>
2025-04-15 21:53:24 -0600

    cambiar icono borrado

--- frontend/src/pages/simon-game/WhiteBoard.tsx
+++ frontend/src/pages/simon-game/WhiteBoard.tsx
@@ -169,12 +169,9 @@ export const Whiteboard: React.FC<WhiteboardProps> = ({
           className={`icon-button ${isErasing ? "button-selected" : ""}`}
         >
           <svg
-            fill="#ffffff"
-            viewBox="0 0 36 36"
-            version="1.1"
-            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+            fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
           >
             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g
@@ -184,22 +181,18 @@ export const Whiteboard: React.FC<WhiteboardProps> = ({
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
-              <title>eraser-solid</title>{" "}
               <path
-                d="M28,32H15.33L19,28.37l-9.9-9.9L3.54,24a1.83,1.83,0,0,0,0,2.6L9,32H3a1,1,0,0,0,0,2H28a1,1,0,0,0,0-2Z"
-                className="clr-i-solid clr-i-solid-path-1"
-              ></path>
-              <path
-                d="M34.08,10.65l-7.3-7.3a1.83,1.83,0,0,0-2.6,0L10.47,17.06l9.9,9.9L34.08,13.25A1.85,1.85,0,0,0,34.08,10.65Z"
-                className="clr-i-solid clr-i-solid-path-2"
+                d="M21.0302 22H13.9902C13.5702 22 13.2402 21.66 13.2402 21.25C13.2402 20.84 13.5802 20.5 13.9902 20.5H21.0302C21.4502 20.5 21.7802 20.84 21.7802 21.25C21.7802 21.66 21.4502 22 21.0302 22Z"
+                fill="#ffffff"
               ></path>{" "}
-              <rect
-                x="0"
-                y="0"
-                width="36"
-                height="36"
-                fill-opacity="0"
-              ></rect>{" "}
+              <path
+                d="M13.64 16.6894C14.03 17.0794 14.03 17.7094 13.64 18.1094L10.66 21.0894C9.55 22.1994 7.77 22.2594 6.59 21.2694C6.52 21.2094 6.46 21.1494 6.4 21.0894L5.53 20.2194L3.74 18.4294L2.88 17.5694C2.81 17.4994 2.75 17.4294 2.69 17.3594C1.71 16.1794 1.78 14.4194 2.88 13.3194L5.86 10.3394C6.25 9.94938 6.88 9.94938 7.27 10.3394L13.64 16.6894Z"
+                fill="#ffffff"
+              ></path>{" "}
+              <path
+                d="M21.1194 10.6414L16.1194 15.6414C15.7294 16.0314 15.0994 16.0314 14.7094 15.6414L8.33937 9.29141C7.94938 8.90141 7.94938 8.27141 8.33937 7.87141L13.3394 2.88141C14.5094 1.71141 16.4294 1.71141 17.5994 2.88141L21.1194 6.39141C22.2894 7.56141 22.2894 9.47141 21.1194 10.6414Z"
+                fill="#ffffff"
+              ></path>{" "}
             </g>
           </svg>
         </button>
